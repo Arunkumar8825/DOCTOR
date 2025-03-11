ee9a0beda5cb5ecbe4a3136ad08ceeb80030a963
Arunkuma <arunsubramaniyan11@gmail.com>
2025-03-11 19:42:09 +0530

    final

--- model/doctorSchema.js
+++ model/doctorSchema.js
@@ -7,7 +7,7 @@ const doctorSchema=new mongoose.Schema({
    address:{type:String,required:true},
    date_of_registration:{type:String,required:true},
    specialist: { type: String, required: true },
-   experience: { type: Number, required: true },
+   experience: { type: Number, required: false},
    visiting_time: { type: String, required: true }
 });
 
